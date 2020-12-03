96a1a75f203f8d3c866789f744ce57513c0f7738
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-03 03:12:29 +0200

    Added scam form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Form, Button } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='mb-4'>Congrats! You won PS5</h1>
+        <Form>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' placeholder='Your Name'></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label controlId='formEmail'> Email address</Form.Label>
+            <Form.Control type='email' placeholder='example@email.com'></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label> Credit Card</Form.Label>
+            <Form.Control type='number' placeholder='123-456-789-000'></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label controlId='formPassword'> Credit Card Password</Form.Label>
+            <Form.Control type='password' placeholder='password'></Form.Control>
+            <Form.Text>We'll never share your email address, "wink, wink"</Form.Text>
+          </Form.Group>
+          <Button variant='danger' type='submit'>Claim Your PS5</Button>
+        </Form>
       </header>
     </div>
   );
